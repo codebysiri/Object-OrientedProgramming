af0ef58975d5458181523ddaacf4b864e46bfd6d
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-04-08 23:09:29 +0900

    Create project

---
+++ src/Chapter02/Ex0207/Starcraft.java
@@ -0,0 +1,48 @@
+package Chapter02.Ex0207;
+
+public class Starcraft {
+    public static void main (String[] args) {
+        // 객체 생성
+        Marine ma = new Marine("레이너", 80);
+        Medic me = new Medic("모랄레스", 60, 60);
+        // 마린의 스팀팩!
+        ma.stimpack();
+        // 메딕의 힐!
+        me.heal(ma);
+    }
+}
+
+class Marine {
+    String name;
+    int hp;
+
+    Marine (String s, int i) {
+        name = s;
+        hp = i;
+    }
+
+    void stimpack() {
+        System.out.printf("[%s]의 스팀팩! HP: %d -> ", name, hp);
+        hp -= 10;
+        System.out.printf("%d\n", hp);
+
+    }
+}
+
+class Medic {
+    String name;
+    int hp;
+    int mp;
+
+    Medic(String s, int i1, int i2) {
+        name = s;
+        hp = i1;
+        mp = i2;
+    }
+
+    void heal(Marine target) {
+        System.out.printf("[%s]의 치유! => [%s] HP(%d -> ", name, target.name, target.hp);
+        target.hp += 10;
+        System.out.printf("%d)", target.hp);
+    }
+}
