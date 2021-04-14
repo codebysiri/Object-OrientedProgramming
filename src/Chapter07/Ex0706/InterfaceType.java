4d125ec037d198fd80e376abc00803ecd76178ac
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-04-14 13:55:20 +0900

    Create project

---
+++ src/Chapter07/Ex0706/InterfaceType.java
@@ -0,0 +1,73 @@
+package Chapter07.Ex0706;
+
+import java.util.ArrayList;
+
+public class InterfaceType {
+    public static void main(String[] args) {
+        // 객체 생성
+        Food food = new Food("족발", 19800);
+        Electronics electronics = new Electronics("에어팟", 199000);
+        Clothing clothing = new Clothing("셔츠", 49900);
+
+        ArrayList<Orderable> list = new ArrayList<>();
+        list.add(food);
+        list.add(electronics);
+        list.add(clothing);
+
+        // 총합 계산
+        int sum = 0;
+        for (int k = 0; k < list.size(); k++) {
+            Orderable o = list.get(k);
+            sum += o.discountedPrice();
+        }
+
+        // 결과 출력
+        System.out.printf("총합: %d원", sum);
+    }
+}
+
+interface Orderable {
+    public int discountedPrice();
+}
+
+class Food implements Orderable {
+    private String name;
+    private int price;
+
+    public Food(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    /* 1. 오버라이딩을 통해, 음식 할인율을 적용하세요. */
+    public int discountedPrice() {
+        return (int)(price * 0.9);
+    }
+}
+
+class Electronics implements Orderable {
+    private String name;
+    private int price;
+
+    public Electronics(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    /* 2. 오버라이딩을 통해, 전자기기 할인율을 적용하세요. */
+    public int discountedPrice() {
+        return (int)(price * 0.8);
+    }
+}
+
+class Clothing implements Orderable {
+    private String name;
+    private int price;
+
+    public Clothing(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    /* 3. 오버라이딩을 통해, 의류 할인율을 적용하세요. */
+    public int discountedPrice() {
+        return (int)(price * 0.7);
+    }
+}
